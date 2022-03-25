6012283ff97c0d4f974923e27cae419b7bd86e73
MatthewYung0 <matthew.yung0@hotmail.com>
2022-03-25 10:28:48 +0100

    .

---
+++ exercises/exercise-one/exercise_twenty.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int number = 0;
+    int base;
+    std::cout << "Enter how many rows of stars to print: ";
+    scanf("%d", &number);
+
+    for (int i = 0; i < number; i) {
+    }
+
+
+}
