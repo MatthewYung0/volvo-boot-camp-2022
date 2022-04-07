c524a7752233ece83e6b250ee613c771227361c6
MatthewYung0 <matthew.yung0@hotmail.com>
2022-04-07 21:37:38 +0200

    .

--- assignments/w14d4a2/week_fourteen_day_four_a_two_matthew_yung.cpp
+++ assignments/w14d4a2/week_fourteen_day_four_a_two_matthew_yung.cpp
@@ -2,13 +2,13 @@
 #include <string>
 #include <bitset>
 
-//Function to concatenate bitfields
+// Function to concatenate bitfields
 template <size_t N1, size_t N2, size_t N3, size_t N4, size_t N5>
 std::bitset<N1 + N2 + N3 + N4 + N5> concat(const std::bitset<N1> &b1,
-                                      const std::bitset<N2> &b2,
-                                      const std::bitset<N3> &b3,
-                                      const std::bitset<N4> &b4,
-                                      const std::bitset<N5> &b5)
+                                           const std::bitset<N2> &b2,
+                                           const std::bitset<N3> &b3,
+                                           const std::bitset<N4> &b4,
+                                           const std::bitset<N5> &b5)
 {
     std::string s1 = b1.to_string();
     std::string s2 = b2.to_string();
@@ -65,40 +65,45 @@ int main()
     std::cout << "\nEnter the front distance as an integer (1 = 150mm ... 7 = 1050mm): ";
     std::cin >> front_input;
     // Error checking for front distance (cannot be less than 150mm or more than 1050/hr since it is 3 bits)
-    if (!checkErrorDistance(front_input)) {
+    if (!checkErrorDistance(front_input))
+    {
         return 0;
     }
     std::bitset<3> front(toBinary(front_input));
     std::cout << front << std::endl;
 
-    //Rear distance bit calculation
+    // Rear distance bit calculation
     std::cout << "\nEnter the rear distance as an integer (1 = 150mm ... 7 = 1050mm): ";
     std::cin >> rear_input;
-    if (!checkErrorDistance(rear_input)) {
+    if (!checkErrorDistance(rear_input))
+    {
         return 0;
     }
     std::bitset<3> rear(rear_input);
     std::cout << rear << std::endl;
 
-    //Right distance bit calculation
+    // Right distance bit calculation
     std::cout << "\nEnter the right distance as an integer (1 = 150mm ... 7 = 1050mm): ";
     std::cin >> right_input;
-    if (!checkErrorDistance(right_input)) {
+    if (!checkErrorDistance(right_input))
+    {
         return 0;
     }
     std::bitset<3> right(right_input);
     std::cout << right << std::endl;
 
-    //Left distance bit calculation
+    // Left distance bit calculation
     std::cout << "\nEnter the left distance as an integer (1 = 150mm ... 7 = 1050mm): ";
     std::cin >> left_input;
-    if (!checkErrorDistance(left_input)) {
+    if (!checkErrorDistance(left_input))
+    {
         return 0;
     }
     std::bitset<3> left(left_input);
     std::cout << left << std::endl;
 
+    // Concantentating bitfields
     std::bitset<16> result = concat(speed, front, rear, right, left);
+    // Printing results
     std::cout << std::endl << result;
-
 }
