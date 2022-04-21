3ebc5cfa73838b9614204684ac4f7fdd7bfa1fbe
MatthewYung0 <matthew.yung0@hotmail.com>
2022-04-21 15:11:23 +0200

    .

--- other/playground.cpp
+++ other/playground.cpp
@@ -1,119 +1,51 @@
-#include <iostream>
-#include <cassert>
-#include <cstdlib>
 #include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
 using namespace std;
 
-void printNumber(int *nptr)
-{
-    cout << *nptr << endl; // dereferences nptr;
+class Student {
+    private:
+    int score;
+    int scores[5];
+    public:
+    void input() {
+        scanf("%d %d %d %d %d", &scores[0], &scores[1],&scores[2], &scores[3], &scores[4]);
+    }
+    int calculateTotalScore() {
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            sum += scores[i];
+        }
+        return sum;
+    }
 };
 
-void printCharacter(char *cptr)
-{
-    cout << *cptr << endl; // dereferences nptr;
-};
-
-void print(void *ptr, char type)
-{
-    switch (type)
-    {
-    case 'i':
-        cout << *((int *)ptr) << endl; // handle int
-        break;
-    case 'c':
-        cout << *((char *)ptr) << endl; // handle float
-        break;
-    }
-}
-
-int getMin(int numbers[], int size)
-{
-    int min = numbers[0];
-    for (int i = i; i < size; i++)
-    {
-        if (numbers[i] < min)
-        {
-            min = numbers[i];
-        }
-    }
-    return min;
-}
-
-int getMax(int numbers[], int size)
-{
-    int max = numbers[0];
-    for (int i = i; i < size; i++)
-    {
-        if (numbers[i] > max)
-        {
-            max = numbers[i];
-        }
-    }
-    return max;
-}
-
-void getMinAndMax(int numbers[], int size, int *min, int *max)
-{
-    *min = numbers[0];
-    *max = numbers[0];
-    for (int i = 0; i < size; i++)
-    {
-        if (numbers[i] > *max)
-        {
-            *max = numbers[i];
-        }
-        if (numbers[i] < *min)
-        {
-            *min = numbers[i];
-        }
-    }
-}
-
-void update(int *a,int *b) {
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
     
-    int sum = *a + *b;
-    int difference = *a - *b;
-    *a = sum;
-    *b = difference;
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
 
-}
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
 
-int main()
-{
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
 
-    /*     int num = 5;
-        char letter = 'A';
-        // printNumber(&num);
-        // printCharacter(&letter);
-        print(&letter, 'c');
-        print(&num, 'i'); */
-
-    // int numbers[5] = {2,3,5,7,9};
-    // // cout << numbers << endl; //address of first element in the array.
-    // // cout << &numbers[0] << endl; // Same as above. Address of element above at index 0.
-    // // cout << numbers[2] << endl; // Prints out the element at index 2.
-    // // cout << *(numbers + 2) << endl; // dereferences, and prints out the element at index 2.
-
-    // int min = 0;
-    // int max = 1;
-
-    // cout << "Before" << endl;
-    // cout << min << endl;
-    // cout << max << endl;
-
-    // getMinAndMax(numbers, 5, &min, &max);
-
-    // cout << "After" << endl;;
-    // cout << min << endl;
-    // cout << max << endl;
-
-    // system("pause>0");
-
-    // int *a = nullptr;
-    // assert(a=nullptr);
-
-    char commas;
+    // print result
+    cout << count;
     
-
+    return 0;
 }
