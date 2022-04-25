fe81561f5f7b2eb2e470f45c6bc418ac74f4949b
MatthewYung0 <matthew.yung0@hotmail.com>
2022-04-25 13:04:22 +0200

    .

---
+++ projects/book_shop_project_final/build/CMakeFiles/book_shop_app.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/book_shop_app.dir/main.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/Book.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/addBookToArray.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/buyBook.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/changeBookDetails.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/checkInput.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/existsInVector.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/exportBookList.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/importBookList.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/printAllBooks.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/printEditDetailsMenu.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/printMenu.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/run.cpp.o"
+  "CMakeFiles/book_shop_app.dir/src/searchForBook.cpp.o"
+  "book_shop_app"
+  "book_shop_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/book_shop_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
