b33f5479506d1e14d15cbddc24d28e89e0644e94
MatthewYung0 <matthew.yung0@hotmail.com>
2022-04-25 13:02:57 +0200

    Merge branch 'main' of https://github.com/MatthewYung0/volvo-boot-camp-2022 into main

--- projects/book_shop_project_final/src/run.cpp
+++ projects/book_shop_project_final/src/run.cpp
@@ -1,26 +1,5 @@
 #include "run.h"
 
-int existsInVector(std::vector<Book> &books, const int books_size)
-{
-    std::string title;
-    std::string author;
-    std::cin.ignore();
-    std::cout << "\nEnter Title Of Book: ";
-    std::getline(std::cin, title);
-    std::cout << "Enter Author Of Book: ";
-    std::getline(std::cin, author);
-    std::cin.clear();
-    for (int i = 0; i < books_size; i++)
-    {
-        // If book is found, index of book in array is returned
-        if (books[i].getTitleName() == title && books[i].getAuthorName() == author)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
 void run()
 {
     int user_selection = -1;
