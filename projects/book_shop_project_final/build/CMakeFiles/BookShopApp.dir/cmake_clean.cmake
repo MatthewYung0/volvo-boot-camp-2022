917e7c9b8c1ab8c60e317452df1f524870a19d04
MatthewYung0 <matthew.yung0@hotmail.com>
2022-04-25 15:24:59 +0200

    .

---
+++ projects/book_shop_project_final/build/CMakeFiles/BookShopApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BookShopApp"
+  "BookShopApp.pdb"
+  "CMakeFiles/BookShopApp.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookShopApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projects/book_shop_project_final/build/CMakeFiles/bookshoplib.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bookshoplib.dir/src/Book.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/addBookToArray.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/buyBook.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/changeBookDetails.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/checkInput.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/existsInVector.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/exportBookList.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/importBookList.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/printAllBooks.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/printEditDetailsMenu.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/printMenu.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/run.cpp.o"
+  "CMakeFiles/bookshoplib.dir/src/searchForBook.cpp.o"
+  "libbookshoplib.pdb"
+  "libbookshoplib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bookshoplib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
