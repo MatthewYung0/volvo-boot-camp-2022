20a049e40e46c07f67a386307471b4d3ddb644f3
MatthewYung0 <matthew.yung0@hotmail.com>
2022-04-27 14:23:57 +0200

    .

---
+++ projects/can_network/KeyboardInput/ncurses/nc.cpp
@@ -0,0 +1,27 @@
+#include <ncurses.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    int ch;
+    nodelay(stdscr, TRUE);
+    for (;;) {
+         if ((ch = getch()) == ERR) {
+         }
+         else {
+             cout << "Input is " << ch << endl;
+         }
+    }
+
+    cout << "Hello World!" << endl;
+
+    endwin();
+    return 0;
+}
